0dc1d200ed131895e105aa0c7ed3b076acf4b984
Devaev Maxim <mdevaev@gmail.com>
2014-10-14 21:49:09 +0400

    Bump version: 0.8 → 0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="elog",
-        version="0.8",
+        version="0.9",
         url="https://github.com/yandex-sysmon/elog",
         license="LGPLv3",
         author="Devaev Maxim",
