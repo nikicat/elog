32565ed0d890a28e605f1768ea5b004c67011c91
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-11-19 14:15:20 +0300

    Bump version: 1.0 → 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="elog",
-        version="1.0",
+        version="1.1",
         url="https://github.com/yandex-sysmon/elog",
         license="LGPLv3",
         author="Devaev Maxim",
